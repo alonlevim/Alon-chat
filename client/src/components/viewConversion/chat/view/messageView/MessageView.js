7913306edc53cc3c847b4a63c3427832a3994ea9
Alon <alonlevim@gmail.com>
2020-02-22 14:55:02 +0200

    Add emoji to message

--- client/src/components/viewConversion/chat/view/messageView/MessageView.js
+++ client/src/components/viewConversion/chat/view/messageView/MessageView.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import { Twemoji } from 'react-emoji-render';
 
 import classes from './MessageView.module.css';
 
 
 const messageView = (props) => {
-    
+
     const allClasses = [classes.MessageView];
     props.me && allClasses.push(classes.Me);
 
     return props.message.trim() !== "" ?
-    <div className={allClasses.join(" ")}>
-        <div className={classes.BorderView}>
-            <p className={classes.Text}>{props.message}</p>
+        <div className={allClasses.join(" ")}>
+            <div className={classes.BorderView}>
+                <Twemoji className={classes.Text} text={props.message.trim()} />
+            </div>
         </div>
-    </div>
-    :
-    null
+        :
+        null
 };
+//https://github.com/banyan/emoji-emoticon-to-unicode/blob/master/emoji-emoticon-to-unicode.js#L94
 
 export default messageView;
