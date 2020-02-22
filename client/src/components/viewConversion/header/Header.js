f27cfc80248eb703ca11aa70799b5b9e894b0532
Alon <alonlevim@gmail.com>
2020-02-23 01:21:38 +0200

    Add avatar member to header

--- client/src/components/viewConversion/header/Header.js
+++ client/src/components/viewConversion/header/Header.js
@@ -16,6 +16,7 @@ const header = (props) => {
         title="back button"
         onClick={props.disableSelectedMember}
         />
+        <div className={classes.AvatarMember} style={{ backgroundImage: `url(${member.image})` }}></div>
         <div className={classes.Column}>
             <div className={classes.Title}>{member && member.name ? member.name : ""}</div>
             <div className={classesOnline.join(" ")}>{isOnline ? "Online" : "Offline"}</div>
