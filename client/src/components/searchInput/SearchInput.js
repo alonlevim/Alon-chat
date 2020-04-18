901f400d46b5072b819ff9c40dc70988c20761d3
Alon <alonlevim@gmail.com>
2020-04-18 20:38:27 +0300

    change logout design

--- client/src/components/searchInput/SearchInput.js
+++ client/src/components/searchInput/SearchInput.js
@@ -5,13 +5,7 @@ import classes from './SearchInput.module.css';
 const searchInput = (props) => {
     return <div className={classes.SearchInput}>
         <input type="text" placeholder="Search in message" onChange={(e)=>{props.updateSearch(e.target.value)}}/>
-        <img
-        className={classes.Exit}
-        src={"http://photos.work-alon.com/exit.svg"}
-        alt="exit"
-        title="exit button"
-        onClick={props.logout}
-        />
+        <p className={classes.Logout} onClick={props.logout}>Log Out</p>
     </div>
 };
 
